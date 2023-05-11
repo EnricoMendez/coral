391ed42a9d5c8b93b4cd62a0b016dcc49e539228
OscarOchoaS <oscar.ochoa4242@gmail.com>
2023-05-10 20:08:54 -0600

    nuevo modelo

--- object_classification/src/object_class_node.py
+++ object_classification/src/object_class_node.py
@@ -75,11 +75,11 @@ class object_cls_node():
     def image_processing(self):
         # Resize the raw image into (224-height,224-width) pixels
         image = self.cv_image
-        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+        image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
         # Change brightness and contrast of image
         image = cv2.addWeighted(image, 3., image, 0., 1.) 
         # Make the image a numpy array and reshape it to the models input shape.
-        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+        image = np.asarray(image, dtype=np.float32).reshape(1, 256, 256, 3)
         # Normalize the image array
         image = (image / 127.5) - 1
         self.object_class(image)

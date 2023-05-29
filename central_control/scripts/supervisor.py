166d9751536a44249506b8ba4adc3a7c5703d182
Enrico <enrico.mendez@outlook.es>
2023-05-29 07:30:12 -0600

    Central_control unused files have been removed

--- central_control/scripts/supervisor.py
+++ central_control/scripts/supervisor.py
@@ -2,14 +2,17 @@
 
 import rospy
 from std_msgs.msg import String
+from gtts import gTTS
 import os
 from std_msgs.msg import Int32
+from std_msgs.msg import Int32MultiArray
 
 class supervisor():
     def __init__(self):
         rospy.on_shutdown(self.cleanup)
         ### suscribers
         rospy.Subscriber("/zimmer/status",String,self.zimmer)
+        rospy.Subscriber("/zimmer/inventory",Int32MultiArray,self.inventory)
         rospy.Subscriber("/move/status",String,self.move)
         rospy.Subscriber("/hand_track/status",String,self.hands)
         rospy.Subscriber("/object_class/status",String,self.object)
@@ -24,8 +27,10 @@ class supervisor():
         self.voice_msg = ['','','','','','']
         self.object_msg = ['','','','','','']
         self.arduino_msg = ['','','','','','']
+        self.invetory_msg = []
+        self.capacity = [0,1,1,2,2,2,1,1]
 
-        r = rospy.Rate(1)
+        r = rospy.Rate(20)
         
         while not rospy.is_shutdown():
             os.system('clear')
@@ -36,8 +41,18 @@ class supervisor():
         list = [list[1],list[2],list[3],list[4],list[5],msg]
         return list
     
+    def talk(self,msg):
+        tts = gTTS(msg, lang='en-us')
+        tts.save('msg.mp3')
+        os.system('mpg123 msg.mp3 > /dev/null 2>&1')
+    
     def zimmer(self,data):
         self.zimmer_msg = self.message_sort(data.data,self.zimmer_msg)
+        answer = data.data
+        self.talk(answer)
+
+    def inventory(self,data):
+        self.invetory_msg = data.data
 
     def move(self,data):
         self.move_msg = self.message_sort(data.data,self.move_msg)
@@ -76,6 +91,11 @@ class supervisor():
         print('Robot control node status:')
         for element in self.move_msg:
             print('\t'+element)
+        print('Inventory:')
+        for i in range(1,len(self.invetory_msg)):
+            print('\tPiece ' + str(i) + ': ' + str(self.invetory_msg[i]) + '/' + str(self.capacity[i]))
+           
+              
         
     def cleanup(self):
         print('Node killed successfully')
